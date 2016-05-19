d58d25b5754c3579ae32e615b4d754243f415f31
Philb37 <raiiko137@gmail.com>
2016-05-19 23:03:11 +0200

    First Commit First window done

--- Vue/MainWindow.xaml.cs
+++ Vue/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Proxar
+namespace Proxar.Vue
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
