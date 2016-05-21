20dcdb36129d8d76dbb9e888e143a93be50d2b46
Orage <clyde.biddle@viacesi.fr>
2016-05-22 01:06:20 +0200

    Change Odb To SQLConnection

--- Modele/BDDUserManagement.cs
+++ Modele/BDDUserManagement.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data;
-
+using System.Data.SqlClient;
 
 namespace Proxar.Modele
 {
@@ -13,15 +13,17 @@ namespace Proxar.Modele
 
         private string rq_sql;                              //Objet string qui contiendra les requêtes SQL
         private string cnx;                                 //Object string qui contiendra la clé de connection a la BDD
-        private System.Data.OleDb.OleDbConnection oCNX;     //Objet Connection de BDD
-        private System.Data.OleDb.OleDbCommand oCMD;        //Objet Command qui executera une requête SQL
-        private System.Data.OleDb.OleDbDataAdapter oDA;     //Objet DataAdapter qui fait le lien entre l'application et la BDD
+        private SqlConnection oCNX;                         //Objet Connection de BDD
+        private SqlCommand oCMD;                            //Objet Command qui executera une requête SQL
+        private SqlDataAdapter oDA;                         //Objet DataAdapter qui fait le lien entre l'application et la BDD
         private System.Data.DataSet oDS;                    //Objet qui stock en local les informations de la BDD
 
         public BDDUserManagement()
         {
-            this.cnx = @"Provider =db-proxar.database.windows.net;Initial Catalog=db_proxar;Persist Security Info=True;User ID=rootProxar;Password=@PR0xAR@";
-            this.oCNX = new System.Data.OleDb.OleDbConnection(this.cnx);
+            this.rq_sql = null;
+            //Changer l'emplacement ou ce trouve votre BDD.
+            this.cnx = @"Data Source=db-proxar.database.windows.net;Initial Catalog=db_proxar;Persist Security Info=True;User ID=rootProxar;Password=@PR0xAR@";
+            this.oCNX = new SqlConnection(this.cnx);
             this.oCMD = null;
             this.oDA = null;
             this.oDS = new DataSet();
@@ -34,10 +36,10 @@ namespace Proxar.Modele
         {
 
             this.oDS = new DataSet();
-            this.rq_sql = "SELECT * FROM db-proxar.db_proxar;";                              //Requête SELECT Paramétrée
-            this.oCMD = new System.Data.OleDb.OleDbCommand(this.rq_sql, this.oCNX);  //Création d'un objet commande qui prend en paramètre la requête SQL et la l'objet de Connexion a la BDD.
-            this.oDA = new System.Data.OleDb.OleDbDataAdapter(this.oCMD);            //Création d'un objet DataAdapter qui prend en paramètre l'objet commande. (DataAdapter et l'interface réel entre l'application et la BDD.
-            this.oDA.Fill(this.oDS, "etudiant");
+            this.rq_sql = "SELECT * FROM db_proxar;";                           //Requête SELECT Paramétrée
+            this.oCMD = new SqlCommand(this.rq_sql, this.oCNX);                      //Création d'un objet commande qui prend en paramètre la requête SQL et la l'objet de Connexion a la BDD.
+            this.oDA = new SqlDataAdapter(this.oCMD);                                //Création d'un objet DataAdapter qui prend en paramètre l'objet commande. (DataAdapter et l'interface réel entre l'application et la BDD.
+            this.oDA.Fill(this.oDS, "Etudiant");                              //Mais bien définir la table a afficher, car le DataSet peut en contenir plusieur.
             int test = this.oDS.Tables.Count;
             return test;
         }
